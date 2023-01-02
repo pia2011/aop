d8e832e3d59ff06d3d4cac1536c198707bd22b4c
pia2011 <sweet_smell@nate.com>
2023-01-02 12:44:59 +0900

    AOP 구현 - 포인트컷 분리

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -13,8 +13,8 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV1.class)
-//@Import(AspectV2.class)
+//@Import(AspectV1.class)
+@Import(AspectV2.class)
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
 //@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
